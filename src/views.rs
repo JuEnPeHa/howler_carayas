7862179e855d36edeaa49fda3d8bc9c8421ac17a
Juan Enrique Enrique <67208274+JuEnPeHa@users.noreply.github.com>
2022-07-04 01:37:30 +0000

    uvghgbibinj

--- src/views.rs
+++ src/views.rs
@@ -2,11 +2,16 @@ use crate::*;
 
 pub trait SaleViews {
     fn get_saled_ids(&self) -> Vec<U64>;
+    fn set_sold_id(&mut self, id: U64);
 }
 
 #[near_bindgen]
 impl SaleViews for Contract {
     fn get_saled_ids(&self) -> Vec<U64> {
         self.minted.keys().into_iter().map(|k| U64(k).clone()).collect()
+    }
+
+    fn set_sold_id(&mut self, id:U64) {
+      
     }
 }
